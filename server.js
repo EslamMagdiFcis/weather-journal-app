cf8c356e6e867389a9e9ac5cf41196692ba093c9
Eslam Magdi <eslam.magdi.fcis@gmail.com>
2020-11-17 20:58:41 +0200

    feat: Setup Server

--- server.js
+++ server.js
@@ -2,18 +2,52 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8000;
+
+const server = app.listen(port, lisening);
+
+function lisening(){
+    console.log(`running on localhost: ${port}`);
+}
+
+// Routing Section
+app.get('/all', retrivalData);
+
+function retrivalData(req, res){
+    res.send(projectData);
+}
+
+app.post('/add', addNewData);
+
+function addNewData(req, res){
+    
+    newEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        user: req.body.user
+    }
+    projectData = newEntry;
+}
